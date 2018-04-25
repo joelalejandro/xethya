a23bacb16f671b5c5d11b6971af229a98d03c7f5
Joel Alejandro Villarreal Bertoldi <joel.a.villarreal@gmail.com>
2018-04-25 07:46:21 -0300

    Item is now Storable

--- src/entity/items/item.i.ts
+++ src/entity/items/item.i.ts
@@ -1,6 +1,7 @@
 import { AbleEntity } from '../able-entity';
 import IPrice from '../../economy/price.i';
+import IStorable from '../inventory/storable.i';
 
-export default interface IItem extends AbleEntity {
+export default interface IItem extends IStorable, AbleEntity {
   readonly baseValue: IPrice;
 };
